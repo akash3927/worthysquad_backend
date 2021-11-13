3d77f2cd37657874b79100062f20aee6ef3b59a2
Akash <akashkothawade77@gmail.com>
2021-11-13 18:21:33 +0530

    axios

--- index.server.js
+++ index.server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv').config();
+const cors = require('cors');
 const conn = require('./src/db/conn');
 const userRoutes = require('./src/routes/auth');
 const eventRoutes = require('./src/routes/events');
@@ -12,7 +13,7 @@ const volunteerRoutes = require('./src/routes/volunteer');
 app.get('/', (req, res) => {
 	res.send('<h1>hello world!</h1>');
 });
-
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use('/api', userRoutes);
